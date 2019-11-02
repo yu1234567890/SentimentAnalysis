985c868862d8cc14c014e64b79698c0fbe8d8678
yuchame <yusheepgit@gmail.com>
2019-11-03 01:01:17 +0900

    new
    new

---
+++ Analysis/SentimentAnalysis/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Microsoft.ML;
+using static Microsoft.ML.DataOperationsCatalog;
+
+namespace SentimentAnalysis
+{
+    class Program
+    {
+        static readonly string _dataPath = Path.Combine(Environment.CurrentDirectory, "Data", "yelp_labelled.txt");
+        static string fromText;
+        static string toText;
+
+        static void Main(string[] args)
+        {
+            Translator trans = new Translator();
+            trans.FromLanguage = "ja";
+            trans.ToLanguage = "en";
+            trans.ModelType = "generalnn";
+            // Azureで取得したキーを設定。
+            trans.OcpApimSubscriptionKey = "";
+
+            // 解析文字列設定
+            fromText = Console.ReadLine();
+            toText = trans.TranslatorText(fromText);
+
+            // mlContextの初期化
+            MLContext mlContext = new MLContext();
+
+            // 学習用データの読み取り
+            TrainTestData splitDataView = LoadData(mlContext);
+
+            // モデル生成
+            ITransformer model = BuildAndTrainModel(mlContext, splitDataView.TrainSet);
+
+            // 文字列の解析
+            UseModelWithBatchItems(mlContext, model);
+
+        }
+
+        public static TrainTestData LoadData(MLContext mlContext)
+        {
+            // テキストデータをIDataViewに格納
+            IDataView dataView = mlContext.Data.LoadFromTextFile<SentimentData>(_dataPath, hasHeader: false);
+
+            // testFraction:テストデータに入る割合
+            TrainTestData splitDataView = mlContext.Data.TrainTestSplit(dataView, testFraction: 0.2);
+
+            return splitDataView;      
+        }
+
+        public static ITransformer BuildAndTrainModel(MLContext mlContext, IDataView splitTrainSet)
+        {
+            var estimator = mlContext.Transforms.Text.FeaturizeText(outputColumnName: "Features", inputColumnName: nameof(SentimentData.SentimentText))
+            .Append(mlContext.BinaryClassification.Trainers.SdcaLogisticRegression(labelColumnName: "Label", featureColumnName: "Features"));
+
+            var model = estimator.Fit(splitTrainSet);
+            return model;
+        }
+
+        public static void UseModelWithBatchItems(MLContext mlContext, ITransformer model)
+        {
+            IEnumerable<SentimentData> sentiments = new[]
+            {
+                new SentimentData
+                {
+                    SentimentText = toText
+                }
+            };
+
+            IDataView batchComments = mlContext.Data.LoadFromEnumerable(sentiments);
+
+            IDataView predictions = model.Transform(batchComments);
+
+            IEnumerable<SentimentPrediction> predictedResults = mlContext.Data.CreateEnumerable<SentimentPrediction>(predictions, reuseRowObject: false);
+
+            foreach (SentimentPrediction prediction  in predictedResults)
+            {
+                Console.WriteLine("翻訳前:" + fromText);
+                Console.WriteLine("翻訳後:" + toText);
+                Console.WriteLine("結果:" + (Convert.ToBoolean(prediction.Prediction) ? "肯定的" : "否定的"));
+                Console.WriteLine("数値:" + prediction.Probability.ToString());
+            }  
+        }
+
+    }
+}
---
+++ Analysis/SentimentAnalysis/Translator.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.IO;
+using System.Net;
+
+namespace SentimentAnalysis
+{
+    public class Translator
+    {
+        private string fromLanguage;
+        /// <summary>
+        /// 翻訳前の言語
+        /// </summary>
+        public string FromLanguage
+        {
+            get { return fromLanguage; }
+            set { fromLanguage = value; }
+        }
+
+        private string toLanguage;
+        /// <summary>
+        /// 翻訳後の言語
+        /// </summary>
+        public string ToLanguage
+        {
+            get { return toLanguage; }
+            set { toLanguage = value; }
+        }
+
+        private string modelType;
+        /// <summary>
+        /// 利用するモデル  ：空白 (=統計的機械翻訳) or generalnn(=ニューラルネットワーク)を指定
+        /// </summary>
+        public string ModelType
+        {
+            get { return modelType; }
+            set { modelType = value; }
+        }
+
+        private string ocpApimSubscriptionKey;
+        /// <summary>
+        /// Translator Text API の Subscription Key（Azure Portal(https://portal.azure.com)から取得）
+        /// </summary>
+        public string OcpApimSubscriptionKey
+        {
+            get { return ocpApimSubscriptionKey; }
+            set { ocpApimSubscriptionKey = value; }
+        }
+
+        public string TranslatorText(string fromText)
+        {
+            string toText = "";
+
+            //URIの定義（Getでパラメータを送信する為、URLに必要なパラメーターを付加）
+            string uri = "https://api.microsofttranslator.com/v2/http.svc/Translate?" +
+            "&text=" + Uri.EscapeDataString(fromText) +
+            "&from=" + fromLanguage +
+            "&to=" + toLanguage +
+            "&category=" + ModelType;
+
+            //httpWebRequestの作成
+            HttpWebRequest httpWebRequest = (HttpWebRequest)WebRequest.Create(uri);
+
+            //Authorizationのためにhttpヘッダーにサブスクリプションキーを埋め込む
+            httpWebRequest.Headers.Add("Ocp-Apim-Subscription-Key:" + ocpApimSubscriptionKey);
+
+            WebResponse response = null;
+
+            //Translator Text APIへのリクエストを実行して結果を取得
+            response = httpWebRequest.GetResponse();
+            using (Stream stream = response.GetResponseStream())
+            {
+                System.Runtime.Serialization.DataContractSerializer dcs =
+                new System.Runtime.Serialization.DataContractSerializer(Type.GetType("System.String"));
+                toText = (string)dcs.ReadObject(stream);
+            }
+
+            return toText;
+        }
+    }
+}
